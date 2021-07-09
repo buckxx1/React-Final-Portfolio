1e1194d55c6550ea3d770b54d59fbd76e6b89bf1
Nicholas Hoban <nickslaptop@Nicholass-MacBook-Pro.local>
2021-07-09 16:44:13 -0400

    probably gonna need some help

--- src/components/topbar/topbar.jsx
+++ src/components/topbar/topbar.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 import "./topbar.scss"
 import {Portrait, Mail} from "@material-ui/icons"
 
-export default function Topbar() {
+export default function Topbar({ menuOpen, setMenuOpen}) {
     return (
-        <div className={"topbar active"}>
+        <div className={"topbar " + (menuOpen && "active")}>
             <div className="wrapper">
                 <div className="left">
                     <a href="#id-intro" classname="logo"> Nick Hoban</a>
@@ -18,7 +18,7 @@ export default function Topbar() {
                     </div>
                 </div>
                 <div className="right">
-                    <div className="hamburger">
+                    <div className="hamburger" onClick={()=> setMenuOpen(!menuOpen)}>
                         <span className="line1"></span>
                         <span className="line2"></span>
                         <span className="line3"></span>
