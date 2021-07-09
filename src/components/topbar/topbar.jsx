1830ad64d24201116667ff9f46f336fa27c89ac3
Nicholas Hoban <nickslaptop@Nicholass-MacBook-Pro.local>
2021-07-08 20:34:28 -0400

    working on the nav bar and its functionality

--- src/components/topbar/topbar.jsx
+++ src/components/topbar/topbar.jsx
@@ -1,16 +1,32 @@
 import React from 'react'
 import "./topbar.scss"
+import {Portrait, Mail} from "@material-ui/icons"
 
 export default function Topbar() {
     return (
-        <div className="topbar">
-            <div className="left">
-                <a href="#id-intro" classname="logo"> Nick Hoban</a>
+        <div className="topbar active">
+            <div className="wrapper">
+                <div className="left">
+                    <a href="#id-intro" classname="logo"> Nick Hoban</a>
+                    <div className="itemContainer">
+                        <Portrait className="icon" />
+                        <span>416-833-4751</span>
+                    </div>
+                    <div className="itemContainer">
+                        <Mail className="icon"/>
+                        <span>nickh1888@gmail.com</span>
+                    </div>
+                </div>
+                <div className="right">
+                    <div className="hamburger">
+                        <span className="line1"></span>
+                        <span className="line2"></span>
+                        <span className="line3"></span>
+                    </div>
+                </div>
             </div>
-
-            <div className="right">
-                this is the right
-            </div>
+            
+                
         </div>
     )
 }
