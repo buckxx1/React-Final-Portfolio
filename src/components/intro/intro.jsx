2a84d4740d16ed7a4d6d0febd2febe2cbfcc39f6
Nicholas Hoban <nickslaptop@Nicholass-MacBook-Pro.local>
2021-07-10 19:16:19 -0400

    cant get ityped working

--- src/components/intro/intro.jsx
+++ src/components/intro/intro.jsx
@@ -1,14 +1,9 @@
-import { InsertInvitation } from '@material-ui/icons';
-import React, { useEffect, useRef } from 'react'
-import "./intro.scss"
+import { useEffect, useRef } from 'react';
+import "./intro.scss";
+
+
 export default function Intro() {
-    const textRef = useRef();
-    useEffect(()=>{
-        init(myElement, {
-            showCursor: false,
-            strings: ["Use with React.js", "yeah"],
-        });
-    },[]);
+    
     return (
         <div className="intro" id="id-intro">
             <div className="left">
@@ -20,7 +15,7 @@ export default function Intro() {
                 <div className="wrapper">
                     <h2>Hello I'm</h2>
                     <h1>Nick Hoban</h1>
-                    <h3>Aspiring <span> Designer</span> </h3>
+                    <h3>Aspiring <span> Developer</span> </h3>
                 </div>
                 <a href="#portfolio">
                     <img  src="assets/down.png" alt="down" width="70px"/>
